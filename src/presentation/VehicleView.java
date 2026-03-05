24297063a0edb9aba656421b11dd024163b88ede
Alberto Tejero <alberto.tejmat@educa.jcyl.es>
2026-03-05 19:04:08 +0100

    manually added code

--- src/presentation/VehicleView.java
+++ src/presentation/VehicleView.java
@@ -37,6 +37,20 @@ public class VehicleView {
         }
 
     }
+    public void visualization(){
+        ArrayList<Vehicle> vehicles;
+        GetAllVehiclesUseCase getAllVehiclesUseCase = new GetAllVehiclesUseCase(VehicleDataRepository.getInstance());
+
+        vehicles = getAllVehiclesUseCase.execute();
+
+        for (Vehicle v : vehicles){
+            System.out.println(v.getId());
+            System.out.println(v.getModel());
+            System.out.println(v.getHp());
+            System.out.println(v.getDoorsNumber());
+            System.out.println(v.getColor());
+        }
+    }
 
     public void deleteAndCheck(String vehicleId){
         ArrayList<Vehicle> vehicles;
