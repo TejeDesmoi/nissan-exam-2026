dcaeec61e4a2ddadbe68d31b1b502a592ad35b10
Alberto Tejero <alberto.tejmat@educa.jcyl.es>
2026-03-05 17:54:23 +0100

    Attributes, constructor and getters/setters created in Vehicle.java

--- src/domain/model/Vehicle.java
+++ src/domain/model/Vehicle.java
@@ -1,8 +1,14 @@
 package domain.model;
 
 public class Vehicle {
+    //region Attributes
     private String id;
-
+    private String model;
+    private String hp;
+    private String doorsNumber;
+    private String color;
+    //endregion
+    //region GetterSetter
     public String getId() {
         return id;
     }
@@ -10,4 +16,48 @@ public class Vehicle {
     public void setId(String id) {
         this.id = id;
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getHp() {
+        return hp;
+    }
+
+    public void setHp(String hp) {
+        this.hp = hp;
+    }
+
+    public String getDoorsNumber() {
+        return doorsNumber;
+    }
+
+    public void setDoorsNumber(String doorsNumber) {
+        this.doorsNumber = doorsNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    //endregion
+    //region Constructor
+
+    public Vehicle(String id, String model, String hp, String doorsNumber, String color) {
+        this.id = id;
+        this.model = model;
+        this.hp = hp;
+        this.doorsNumber = doorsNumber;
+        this.color = color;
+    }
+
+    //endregion
 }
